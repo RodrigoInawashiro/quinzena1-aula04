1e91c4398fa18633da73ab08a887d2e08e5369bc
Rodrigo Inawashiro <rodrigoinaw@hotmail.com>
2021-05-25 21:48:49 -0300

    exercicios Operadores

---
+++ quinzena1/operadores/template_javascript/index.js
@@ -0,0 +1,70 @@
+//Exercícios de interpretação de código
+
+//Exercício 1
+
+/*a.false
+b.false
+c.true
+d.boolean*/
+
+//Exercício 2
+
+/* Como o prompt mostra o primeiroNumero e segundoNumero como strings ele junta(concatena) 
+e não soma por não considerá-los numeros.Teria que declarar: (number(primeiroNumero) + (number(segundoNumero)*/
+/* Seria impresso por exemplo 2 + 10 = 210*/
+
+//Exercício 3
+
+/*Deverá colocar antes de prompt, a funcionalidade Number para não ser lido como string.*/
+
+//Exercícios de escrita de código
+
+//Exercício 1
+
+/*let idade = Number(prompt("Qual a sua idade?"))
+let idadeDoSeuMelhorAmigo = Number(Prompt("Qual a idade de seu amigo?"))
+console.log("Sua idade é maior do que a do seu amigo?", idade > idadeDoSeuMelhorAmigo)
+console.log(idade - idadeDoSeuMelhorAmigo)*/
+
+
+//Exercício 2
+
+/*let numeroPar = (Number(prompt("Digite um numero par")))
+let resto = numeroPar % 2
+console.log(resto)*/
+
+//Numeros pares terão resto igual a 0
+//Numeros impares terão resto igual a 1
+
+
+//Exercício 3
+
+/*let idade = (Number(prompt("Qual sua idade?")))
+const idadeMeses = idade * 12
+const idadeDias = idade * 365
+const idadeHoras = idade * 8760
+console.log("Idade em meses:",idadeMeses)
+console.log("Idade em dias:",idadeDias)
+console.log("Idade em horas:",idadeHoras)*/
+
+
+//Exercício 4
+
+/*const numero1 = (Number(prompt("Digite um numero")))
+const numero2 = (Number(prompt("Digite outro numeor")))
+
+console.log(numero1>numero2)
+console.log(numero1 == numero2)
+console.log(numero1 & numero2) == 0
+console.log(numero2 & numero1) == 0*/
+
+
+
+
+
+
+
+
+
+
+
